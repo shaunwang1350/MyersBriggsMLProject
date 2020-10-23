2e48bfb5f6a9c5e8e4feccc7889c662bb1767180
jjin92 <jjin92.2014@gmail.com>
2020-10-23 18:56:56 -0400

    edit app.py to include all four dimensions

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from sklearn.linear_model import LogisticRegression
 app = Flask(__name__)
 
 model_EI = pickle.load(open('Resources/model_weights/model_EI.pkl', 'rb'))
-# model_NS = pickle.load(open('Resources/model_weights/model_NS.pkl', 'rb'))
-# model_FT = pickle.load(open('Resources/model_weights/model_FT.pkl', 'rb'))
-# model_JP = pickle.load(open('Resources/model_weights/model_JP.pkl', 'rb'))
+model_NS = pickle.load(open('Resources/model_weights/model_NS.pkl', 'rb'))
+model_FT = pickle.load(open('Resources/model_weights/model_FT.pkl', 'rb'))
+model_JP = pickle.load(open('Resources/model_weights/model_JP.pkl', 'rb'))
 
 vectorizer = pickle.load(open('Resources/vectorizer/vectorizer.pkl', 'rb'))
 
@@ -33,19 +33,19 @@ def predict():
 
     # Product the result for four axes
     prediction_EI = model_EI.predict(userInput_Vectorized)
-    # prediction_NS = model_NS.predict(userInput_Vectorized)
-    # prediction_FT = model_FT.predict(userInput_Vectorized)
-    # prediction_JP = model_JP.predict(userInput_Vectorized)
+    prediction_NS = model_NS.predict(userInput_Vectorized)
+    prediction_FT = model_FT.predict(userInput_Vectorized)
+    prediction_JP = model_JP.predict(userInput_Vectorized)
 
     # convert the prediction result from 1 and 0 to letters
     output_EI = 'E' if prediction_EI == 0 else "I"
-    # output_NS = 'N' if prediction_NS == 0 else "S"
-    # output_FT = 'F' if prediction_FT == 0 else "T"
-    # output_JP = 'J' if prediction_JP == 0 else "P"
+    output_NS = 'N' if prediction_NS == 0 else "S"
+    output_FT = 'F' if prediction_FT == 0 else "T"
+    output_JP = 'J' if prediction_JP == 0 else "P"
 
     return render_template('index.html',
-        prediction_text=f'{output_EI}')
-        # {output_NS}{output_FT}{output_JP}
+        prediction_text=f'{output_EI}{output_NS}{output_FT}{output_JP}')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
